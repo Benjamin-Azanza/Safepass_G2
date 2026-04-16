062cad6915a5f4bf1a647285f7ddac1f8fbf7f04
Benjamin-Azanza <benjamin.azanza@gmail.com>
2026-04-16 18:15:33 -0500

    ui state

--- app/src/main/java/com/example/safepass_g2/ui/registro/RegistroState.kt
+++ app/src/main/java/com/example/safepass_g2/ui/registro/RegistroState.kt
@@ -1,2 +1,22 @@
 package com.example.safepass_g2.ui.registro
 
+
+sealed class RegistroState {
+
+     //Estado inicial: el usuario aún no ha ingresado datos.
+
+    object Idle : RegistroState()
+
+
+     //Estado exitoso: los datos fueron validados correctamente.
+
+    data class Success(
+        val resumen: String
+    ) : RegistroState()
+
+
+     //Estado de error: datos inválidos o incompletos.
+    data class Error(
+        val mensaje: String
+    ) : RegistroState()
+}
